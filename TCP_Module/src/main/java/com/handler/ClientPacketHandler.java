ef59e40f035b99487b71ed7affd9d226110d3d57
LQ1-1 <1522788291@qq.com>
2026-04-19 20:56:34 +0800

    working on Netty Frame

--- TCP_Module/src/main/java/com/handler/ClientPacketHandler.java
+++ TCP_Module/src/main/java/com/handler/ClientPacketHandler.java
@@ -1,11 +1,31 @@
 package com.handler;
 
+import com.client.ClientConnectionManager;
+import com.common.protocol.Packet;
+import com.service.ClientTransferService;
 import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
 
 @Component
 @ChannelHandler.Sharable
-public class ClientPacketHandler
+public class ClientPacketHandler extends SimpleChannelInboundHandler<Packet>
 {
+    private final ClientConnectionManager clientConnectionManager;
+    private final ClientTransferService clientTransferService;
+
+    public ClientPacketHandler(@Lazy ClientConnectionManager clientConnectionManager, @Lazy ClientTransferService clientTransferService)//延迟加载注解，不要在容器启动时立刻创建这个依赖对象，而是在真正用到它的时候再创建。
+    {
+        this.clientConnectionManager = clientConnectionManager;
+        this.clientTransferService = clientTransferService;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Packet msg)throws Exception
+    {
+
+    }
 
 }
---
+++ TCP_Module/src/main/java/com/handler/ServerPacketHandler.java
@@ -0,0 +1,30 @@
+package com.handler;
+
+
+import com.common.protocol.Packet;
+import com.service.ServerRoutingService;
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+@ChannelHandler.Sharable
+public class ServerPacketHandler extends SimpleChannelInboundHandler<Packet>
+{
+    private final ServerRoutingService serverRoutingService;
+
+    public ServerPacketHandler(ServerRoutingService serverRoutingService)
+    {
+        this.serverRoutingService = serverRoutingService;
+    }
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, Packet msg)
+    {
+
+    }
+
+
+}
