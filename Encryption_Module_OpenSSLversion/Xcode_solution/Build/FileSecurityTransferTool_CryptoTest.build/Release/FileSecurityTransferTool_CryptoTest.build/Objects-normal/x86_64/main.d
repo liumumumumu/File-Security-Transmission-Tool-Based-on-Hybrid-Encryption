e93600bd164045afee0777d0715d7b8f3b541e36
LQ1-1 <1522788291@qq.com>
2026-05-15 11:50:47 +0800

    update gitignore and remove unnecessary files

--- Encryption_Module_OpenSSLversion/Xcode_solution/Build/FileSecurityTransferTool_CryptoTest.build/Release/FileSecurityTransferTool_CryptoTest.build/Objects-normal/x86_64/main.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/zero/Documents/c_xcode_code/FileSecurityTransferTool_CryptoTest/FileSecurityTransferTool_CryptoTest/main.cpp \
-  /Users/zero/Documents/c_xcode_code/FileSecurityTransferTool_CryptoTest/FileSecurityTransferTool_CryptoTest/httplib.h
